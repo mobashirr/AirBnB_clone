8c45c2a0403c421c296f72454045bb60e7e58d65
sozoia <zfzg2232@gmail.com>
2024-03-09 15:33:40 +0300

    task 6

--- console.py
+++ console.py
@@ -1,23 +1,19 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """HBNBCommand class"""
+    """Simple command interpreter example."""
 
-    prompt = "(hbnb) "
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        return True  # Returning True will exit the command loop
-
-    def do_EOF(self, arg):
-        pass
-
-    def help_EOF(self):
-        print("EOF command to exit the program")
+        """Quit command to exit the program"""
+        return True
 
     def help_quit(self):
         print("Quit command to exit the program")
 
     def emptyline(self):
+        """Called when an empty line is entered."""
         pass
 
 if __name__ == '__main__':
