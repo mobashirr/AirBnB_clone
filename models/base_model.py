9d996351d7f658f1039ba990a5c3c9963681e786
sozoia <zfzg2232@gmail.com>
2024-06-10 23:28:00 +0300

    logic for deserelization

--- models/base_model.py
+++ models/base_model.py
@@ -11,22 +11,32 @@ import uuid, datetime
 
 
 class BaseModel:
-    """
+
+
+    def __init__(self, *args, **kwargs):
+        """
         Initiating an instance...
 
-        id: a uniqe identifier of the instance
+        id: a unique identifier of the instance
 
         created_at: date of the instance in which it has
         been created.
 
         updated_at: date of the instance in which it has
         been updated.
-    """
-
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        self.__dict__[key] = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
+    
 
     def __str__(self):
         return f"[{self.__class__.__name__ }] ({self.id}) {self.__dict__}"
